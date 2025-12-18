ef32f30db15215b9225ae4fc2ef558dd75a7570c
Eduard Kyrychenko <eduardkyrychenko923@gmail.com>
2025-12-18 17:00:14 +0200

    correct naming

--- src/locations/repository.py
+++ src/locations/repository.py
@@ -1,18 +1,18 @@
 from sqlalchemy import func
 from sqlalchemy.orm import Session
-from src.models import Location, Trip, Payment
+from src.models import CarLocation, Trip, Payment
 
 class LocationRepository:
     def get_all(self, db: Session):
-        return db.query(Location).all()
+        return db.query(CarLocation).all()
 
     def get_financial_report(self, db: Session):
         return db.query(
-            Location.address.label("address"),
+            CarLocation.address.label("address"),
             func.count(Trip.trip_id).label("total_trips"),
             func.sum(Payment.amount).label("total_revenue"),
             func.avg(Payment.amount).label("avg_trip_cost")
-        ).join(Trip, Location.location_id == Trip.end_location_id) \
+        ).join(Trip, CarLocation.CarLocation_id == Trip.end_CarLocation_id) \
          .join(Payment, Trip.trip_id == Payment.trip_id) \
-         .group_by(Location.address) \
+         .group_by(CarLocation.address) \
          .all()
--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from src.users import users_router
 from src.auth import auth_router
 from src.trips import trips_router
+from src.cars import cars_router
+from src.locations import locations_router
 from src.bookings import bookings_router
 from src.driver_licenses import driver_licenses_router
 from src.config import config
@@ -13,6 +15,8 @@ app = FastAPI(title=config.APP_NAME)
 app.include_router(users_router)
 app.include_router(auth_router)
 app.include_router(trips_router)
+app.include_router(cars_router)
+app.include_router(locations_router)
 app.include_router(bookings_router)
 app.include_router(driver_licenses_router)
 
